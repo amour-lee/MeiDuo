61c0365c78c4dc4255a6e8a44887565bdcead643
Lee <15537186765@163.com>
2018-09-06 10:56:31 +0800

    注册之自定义用户模型类

--- meiduo_mall/meiduo_mall/settings/dev.py
+++ meiduo_mall/meiduo_mall/settings/dev.py
@@ -192,3 +192,7 @@ REST_FRAMEWORK = {
     # 异常处理
     'EXCEPTION_HANDLER': 'meiduo_mall.utils.exceptions.exception_handler',
 }
+
+# 指定默认的用户模型类
+# 注意：语法规则必须是'应用名.模型类'
+AUTH_USER_MODEL = 'users.User'
